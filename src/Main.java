cb7cd5d359f77b057c7da2125a4aeb64ebb44d94
MiguelVL16 <miguelvlaperal@gmail.com>
2024-05-10 19:34:37 +0200

    Nueva clase Libro

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,86 @@
-package Ejercicio2;
+package Ejercicio1;
 
-import java.io.File;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        String titulo, autor;
+        int ejemplares;
 
-        // Solicita al usuario que introduzca la ruta del archivo o directorio
-        System.out.println("Introduce la ruta del fichero o directorio:");
-        String ruta = scanner.nextLine();
+        //se crea el objeto libro1 utilizando el constructor con parámetros
+        Libro libro1 = new Libro("El quijote", "Cervantes", 1, 0);
+        //se crea el objeto libro2 utilizando el constructor por defecto
+        Libro libro2 = new Libro();
 
-        // Crea una instancia de File con la ruta introducida
-        File file = new File(ruta);
+        System.out.print("Introduce titulo: ");
+        titulo = sc.nextLine();
+        System.out.print("Introduce autor: ");
+        autor = sc.nextLine();
+        System.out.print("Numero de ejemplares: ");
+        ejemplares = sc.nextInt();
 
-        // Verifica si el fichero/directorio existe
-        if (file.exists()) {
-            System.out.println("El fichero/directorio existe.");
+        //se asigna a libro2 los datos pedidos por teclado.
+        //para ello se utilizan los métodos setters
+        libro2.setTitulo(titulo);
+        libro2.setAutor(autor);
+        libro2.setEjemplares(ejemplares);
 
-            // Verifica si es un directorio o un fichero
-            if (file.isDirectory()) {
-                System.out.println("Es un directorio.");
-            } else if (file.isFile()) {
-                System.out.println("Es un fichero.");
-                // Muestra información del fichero
-                System.out.println("Nombre: " + file.getName());
-                System.out.println("Tamaño: " + file.length() + " bytes");
-                if (file.canRead()) {
-                    System.out.println("Permisos de lectura: Sí");
-                } else {
-                    System.out.println("Permisos de lectura: No");
-                }
-                if (file.canWrite()) {
-                    System.out.println("Permisos de escritura: Sí");
-                } else {
-                    System.out.println("Permisos de escritura: No");
-                }
-            }
+        //se muestran por pantalla los datos del objeto libro1
+        //se utilizan los métodos getters para acceder al valor de los atributos
+        System.out.println("Libro 1:");
+        System.out.println("Titulo: " + libro1.getTitulo());
+        System.out.println("Autor: " + libro1.getAutor());
+        System.out.println("Ejemplares: " + libro1.getEjemplares());
+        System.out.println("Prestados: " + libro1.getPrestados());
+        System.out.println();
+
+        //se realiza un préstamo de libro1. El método devuelve true si se ha podido
+        //realizar el préstamo y false en caso contrario
+        if (libro1.prestamo()) {
+            System.out.println("Se ha prestado el libro " + libro1.getTitulo());
         } else {
-            System.out.println("El fichero/directorio no existe.");
+            System.out.println("No quedan ejemplares del libro " + libro1.getTitulo() + " para prestar");
         }
 
-    }
+        //se realiza una devolución de libro1. El método devuelve true si se ha podido
+        //realizar la devolución y false en caso contrario
+        if (libro1.devolucion()) {
+            System.out.println("Se ha devuelto el libro " + libro1.getTitulo());
+        } else {
+            System.out.println("No hay ejemplares del libro " + libro1.getTitulo() + " prestados");
+        }
+
+        //se realiza otro préstamo de libro1
+        if (libro1.prestamo()) {
+            System.out.println("Se ha prestado el libro " + libro1.getTitulo());
+        } else {
+            System.out.println("No quedan ejemplares del libro " + libro1.getTitulo() + " para prestar");
+        }
+
+        //se realiza otro préstamo de libro1. En este caso no se podrá realizar ya que
+        //solo hay un ejemplar de este libro y ya está prestado. Se mostrará por
+        //pantalla el mensaje No quedan ejemplares del libro…
+        if (libro1.prestamo()) {
+            System.out.println("Se ha prestado el libro " + libro1.getTitulo());
+        } else {
+            System.out.println("No quedan ejemplares del libro " + libro1.getTitulo() + " para prestar");
+        }
+        //mostrar los datos del objeto libro1
+        System.out.println("Libro 1:");
+        System.out.println("Titulo: " + libro1.getTitulo());
+        System.out.println("Autor: " + libro1.getAutor());
+        System.out.println("Ejemplares: " + libro1.getEjemplares());
+        System.out.println("Prestados: " + libro1.getPrestados());
+        System.out.println();
+
+        //mostrar los datos del objeto libro2
+        System.out.println("Libro 2:");
+        System.out.println("Titulo: " + libro2.getTitulo());
+        System.out.println("Autor: " + libro2.getAutor());
+        System.out.println("Ejemplares: " + libro2.getEjemplares());
+        System.out.println("Prestados: " + libro2.getPrestados());
+        System.out.println();
+    }//Fin de la clase Fraccion
 }
